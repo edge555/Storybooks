268e35c66003626dfaffec7a630515de5c57e778
Shoaib Ahmed <shoaib27@student.sust.edu>
2020-05-21 05:22:12 +0600

    Update

--- routes/auth.js
+++ routes/auth.js
@@ -6,4 +6,9 @@ router.get('/google',passport.authenticate('google',{
     scope:['profile','email']
 }));
 
+router.get('/google/callback', 
+passport.authenticate( 'google', { failureRedirect: '/' }),(req, res) => {
+        res.redirect('/dashboard');
+    });
+
 module.exports = router;
