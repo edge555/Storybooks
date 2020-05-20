e7d791edec3809248a29d55b78d8ebb3caaec9c9
Shoaib Ahmed <shoaib27@student.sust.edu>
2020-05-21 01:37:34 +0600

    Initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+const express= require('express');
+const mongoose = require('mongoose');
+const app = express();
+
+app.get('/',(req,res)=>{
+    res.send('ok');
+})
+
+const port = process.env.PORT || 3000;
+
+app.listen(port,()=>{
+    console.log("Server started");
+});
