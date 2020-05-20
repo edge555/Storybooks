4f686c0184f4dedade8e7d5e4fb64811a460392e
Shoaib Ahmed <shoaib27@student.sust.edu>
2020-05-21 02:46:03 +0600

    Add keys and Google Auth

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
 const express= require('express');
 const mongoose = require('mongoose');
+const passport= require('passport');
+// Passport config
+require('./config/passport')(passport)
+
+// Load routes
+const auth = require('./routes/auth');
+
 const app = express();
 
 app.get('/',(req,res)=>{
     res.send('ok');
-})
+});
+
+// Use routes
+app.use('/auth',auth);
 
 const port = process.env.PORT || 3000;
 
---
+++ config/keys.js
@@ -0,0 +1,7 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://admin-shoaib:test123@storybooks-dev-u0p9q.mongodb.net/test?retryWrites=true&w=majority",
+  googleClientID:
+    "272126512458-mhhff64vbu2far4bm34v8kr4ps837mk2.apps.googleusercontent.com",
+  googleClientSecret: "rRRKqp89lbhsnHKJe-Vw4B7P"
+};
